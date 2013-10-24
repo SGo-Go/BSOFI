882ed79a5344c8c0045ac59681a36efe10f17580
SGo-Go <sgogolenko@ucdavis.edu>
2013-10-24 20:07:20 +0300

    Changes in Dirac benchmarking script to make it able be run with old Python interpreters

--- README.md
+++ README.md
@@ -1,52 +1,57 @@
-==================
+======================================================================
 BSOF/I README FILE
-==================
 
 Structured Orthogonal Inversion Codes for Block p-Cyclic Matrices
+======================================================================
 
-=====
+-----------------------------------------------------------------
 
 This is a set of pure C99 codes for 
 block structured factorization and inversion (BSOF/I)
 of block p-cyclic matrices on CPU+GPU platforms.
 
-* To COMPILE BSOF/I codes, create the settings.make file to indicate where 
+* To **COMPILE** BSOF/I codes, create the settings.make file to indicate where 
   CUDA, CPU BLAS, MAGMA, and LAPACK are installed on your system. Example
-  are given in zwolf.make, showing how to link to 
+  is given in zwolf.make, showing how to link to 
   MKL, CuBLAS and MAGMA on zWolf platform in UC Davis.  
    
-* To TUNE CPU+GPU codes, you can modify the parameters 
-  inserted in file 'include/bsofi_params.h'. The default 
+* To **TUNE** CPU+GPU codes, you can modify the parameters 
+  inserted in file *include/bsofi_params.h*. The default 
   values are tuned for zWolf. 
-  Parameters KAPPA_* control workload distribution between host and device.
-  Parameters *_SWITCH_* specify when to switch between CPU and hybrid codes. 
+  Parameters `KAPPA_\*` control workload distribution between host and device.
+  Parameters `\*_SWITCH_\*` specify when to switch between CPU and hybrid codes. 
 
-* To TEST BSOF/I, compile codes and run 'test'.  
+* To **TEST** BSOF/I, compile codes and run *test*.  
 
   Note that on some systems the experimental code must be linked against 
   sequential MKL.  If you notice excessively poor performance, change 
   make.inc to link against sequential MKL.
 
-* To  CALL BSOF/I subroutines in own codes, see examples 
-  'ftest_hybrid.f90', 'ftest_cpu.f90' (Fortran) and 'ctest.c' (C/C++).  
+* To  **CALL** BSOF/I subroutines in own codes, see examples 
+  *ftest_hybrid.f90*, *ftest_cpu.f90* (Fortran) and *ctest.c* (C/C++).  
   The general scheme for Fortran is shown below
 
-  integer  :: nnb, lw
-  integer  ::handler(10)
+    integer  :: nnb, lw
+    integer  ::handler(10)
+    
+    ! Initialize device and allocate GPU memory
+    call hybridXinitH(n, nb, nnb, handler)
+    
+    ! Create working space on CPU
+    lw = hybridXbsofiLWorkH(n, nb, nnb, -1)
+    allocate(W1(lw))
+    
+    ! Inversion
+    call hybridXbsoftriH(handler, n, nb, A, nnb, tauBsofi, W1, lw, info)
+    call hybridXbsoiH(handler, n, nb, A, nnb, tauBsofi, W1, lw, info)
+    
+    ! Shutdown device and deallocate GPU memory
+    call hybridXfinalizeH(handler)
 
-  ! Initialize device and allocate GPU memory
-  call hybridXinitH(n, nb, nnb, handler)
+For more INFORMATION, please refer to the technical report:
 
-  ! Create working space on CPU
-  lw = hybridXbsofiLWorkH(n, nb, nnb, -1)
-  allocate(W1(lw))
-
-  ! Inversion
-  call hybridXbsoftriH(handler, n, nb, A, nnb, tauBsofi, W1, lw, info)
-  call hybridXbsoiH(handler, n, nb, A, nnb, tauBsofi, W1, lw, info)
-
-  ! Shutdown device and deallocate GPU memory
-  call hybridXfinalizeH(handler)
-
-For more INFORMATION, please refer to the technical report
-
+* Sergiy Gogolenko and Zhaojun Bai
+[A Structured Orthogonal Inversion of Block 
+p-Cyclic Matrices on Multicores with GPU Accelerators]
+(http://www.cs.ucdavis.edu/research/tech-reports/2012/CSE-2013-78.pdf),
+Techn. rep. No. CSE-2013-78, 2013
--- bench/dirac_bench.py
+++ bench/dirac_bench.py
@@ -12,17 +12,72 @@
 
 import os
 
-L = 4
+############################################################
+# Parameters
+############################################################
+
+L    = 4
+Lmax = 44
 n_list = (1024, 512, 256)
 nthreads = 12
 implementations = ('hybrid',)# 'cpu')
 platform = 'dirac_fermi'
 
 ofolder = r"./results/%s" % platform
-cmd_all = r"./bench/{implement}/bench -t {ntests} -n {blksize} -L {blknum}"\
-    " -f {ofolder}/{blksize:04d}_{nthreads:02d}.blk"
-scmd_all = r"./bench/%s/bench -t %s -n %s -L %s -f %s/%04i_%02i.blk"
 
+############################################################
+def call_minimalistic():
+    """
+    Implementation of job running 
+    with minimalistic requirements to Python interpreter
+    """
+    cmd_format          = r"./bench/%s/bench -t %s -n %s -L %s -f %s/%04i_%02i.blk"
+    job_file_format     = r"%s/%s%04i.job"
+    job_template_format = r"config/jobs/%s.sh.in"
+
+    ntests = Lmax*(1024/n) - 3
+    cmd = cmd_format % (implementation, ntests, n, L, 
+                     ofolder_local, n, nthreads)
+    job_template_handle = open(job_template_format % platform, "r")
+    job_script = job_template_handle.read()
+    job_template_handle.close()
+    job_script = job_script.replace(r"@cmd@", cmd)
+    # print job_script
+    job_file = job_file_format % (ofolder_local, platform,n)
+    job_script_handle = open(job_file, "w")
+    job_script_handle.write(job_script)
+    job_script_handle.close()
+    #os.system(r"qsub -S /bin/bash %s" % job_file)
+
+############################################################
+def call_modern():
+    """
+    Implementation of job running 
+    for Python version >= 2.5
+    """
+    cmd_format = r"./bench/{implement}/bench -t {ntests} -n {blksize} -L {blknum}" \
+        " -f {ofolder}/{blksize:04d}_{nthreads:02d}.blk"
+    job_file_format = r"{ofolder}/{platform}{blksize:04d}.job"
+    job_template_format = r"config/jobs/{platform}.sh.in"
+
+    ntests = Lmax*(1024/n) - 3
+    cmd = cmd_format.format(implement = implementation, 
+                         ofolder = ofolder_local,
+                         nthreads=nthreads, ntests=ntests, 
+                         blknum=L, blksize=n)
+    with open(job_template_format.format(platform=platform), "r") as job_template_handle:
+        job_script = job_template_handle.read()
+    job_script = job_script.replace(r"@cmd@", cmd)
+    # print job_script
+    job_file = job_file_format.format(ofolder = ofolder_local, 
+                                      platform = platform, blksize = n)
+    with open(job_file, "w") as job_script_handle:
+        job_script_handle.write(job_script)
+    #os.system(r"qsub -S /bin/bash %s" % job_file)
+
+############################################################ 
+# Cycle over commands to run
+############################################################ 
 if not os.path.exists(ofolder): os.makedirs(ofolder)
 for implementation in implementations:
     ofolder_local = os.path.join(ofolder, implementation)
@@ -32,18 +87,4 @@ for implementation in implementations:
     # os.environ['MKL_NUM_THREADS'] = str(nthreads)
 
     for n in n_list:
-        ntests = 44*(1024/n) - 3
-        # cmd = cmd_all.format(implement = implementation, 
-        #                      ofolder = ofolder_local,
-        #                      nthreads=nthreads, ntests=ntests, 
-        #                      blknum=L, blksize=n)
-        cmd = scmd_all % (implementation, ntests, n, L, 
-                          ofolder_local, n, nthreads)
-        with open(r"config/jobs/%s.sh.in" % platform, "r") as job_script_handle:
-            job_script = job_script_handle.read()
-        job_script = job_script.replace(r"@cmd@", cmd)
-        print job_script
-        job_file = r"%s/%s%04i.job" % (ofolder_local, platform,n)
-        with open(job_file, "w") as job_script_handle:
-            job_script_handle.write(job_script)
-        os.system(r"qsub -S /bin/bash %s" % job_file)
+        call_minimalistic() # call_modern() #
